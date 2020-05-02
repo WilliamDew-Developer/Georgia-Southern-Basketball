9a5f131cf94357d3014473ba4e7508901fac0b95
William Dew <wd01248@georgiasouthern.edu>
2020-05-02 18:38:30 -0400

    Updated index.html
    *added button that plays animation on home screen
    *hailsouthern.js added for button

---
+++ hailsouthern.js
@@ -0,0 +1,6 @@
+//hailsouthern.js
+
+//function to change content on webpage
+function hailSouthern(){
+	document.getElementById("hailsouthern").innerHTML = "<img src = \"./hailsouthern.gif\" height = \"700\" width = \"700\">";
+}
