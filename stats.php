2cc8320d6a44a309ce7983abe5955e17d9cdcd88
William Dew <wd01248@georgiasouthern.edu>
2020-05-02 18:15:19 -0400

    Completed Stats Page
    *deleted stats.html
    *stats.php now works properly
    *user can pick an option from dropdown menu and submit option to run proper query

--- stats.php
+++ stats.php
@@ -30,22 +30,21 @@
 		<!--Prompt user to pick a year-->
 		<div class = "seasonPick">
 			<h2>Men's Basketball Cumulative Statistics</h2>
-			<form action = "#" method = "post" id = "statForm" name = "statForm">
+			<form action = "#" method = "post" id = "statForm">
 				<label for = "season">Pick a year:</label>
 				<select id ="season" name = "season">
-					<option style = "display:none" disabled selected option >-- Pick A Season --</option>
-					<option value = "2019-20">2019-20</option>
-					<option value = "2018-19">2018-19</option>
-					<option value = "2017-18">2017-18</option>
-					<option value = "2016-17">2016-17</option>
-					<option value = "2015-16">2015-16</option>
-					<option value = "2014-15">2014-15</option>
-					<option value = "2013-14">2013-14</option>
-					<option value = "2012-13">2012-13</option>
-					<option value = "2011-12">2011-12</option>
-					<option value = "2010-11">2010-11</option>
+					<option value = "2019-20 stats">2019-20</option>
+					<option value = "2018-19 stats">2018-19</option>
+					<option value = "2017-18 stats">2017-18</option>
+					<option value = "2016-17 stats">2016-17</option>
+					<option value = "2015-16 stats">2015-16</option>
+					<option value = "2014-15 stats">2014-15</option>
+					<option value = "2013-14 stats">2013-14</option>
+					<option value = "2012-13 stats">2012-13</option>
+					<option value = "2011-12 stats">2011-12</option>
+					<option value = "2010-11 stats">2010-11</option>
 				</select>
-				<input type = "submit" value = "Submit" name = "submit">
+				<input type = "submit" name = "submit" value = "Submit">
 			</form>
 		</div>
 		
@@ -57,68 +56,69 @@
 				$password = "";
 				$dbname = "project";
 				
-
-				// Create connection
-				$conn = mysqli_connect($servername, $username, $password, $dbname);
-				// Check connection
-				if (!$conn) {
-				    die("Connection failed: " . mysqli_connect_error());
-				}
-
 				if(isset($_POST['submit'])){
-					$table = $_POST['statForm'];// . " stats";//concatenate the user input with the word "stats" for the query
+					$table = strval($_POST['season']);//get user's submission: ex. 2014-15 stats
+					$season = str_replace("stats","Season",$table);//saves table variable in different format: ex. 2014-15 Season
+					echo "<h3>". $season . "</h3>";//display table name
+					// Create connection
+					$conn = mysqli_connect($servername, $username, $password, $dbname);
+					// Check connection
+					if (!$conn) {
+					    die("Connection failed: " . mysqli_connect_error());
+					}
+
+					$sql = "SELECT jersey, player, yr, pos, ht, gp, `fg%`, `3fg%`, `ft%`, avgpts, avgreb, ast, stl, blk, fouls FROM `{$table}`" ;
+					$result = mysqli_query($conn, $sql);
+
+					if (mysqli_num_rows($result) > 0) {
+						echo 
+						"<table>
+						<tr>
+							<th>Jersey</th>
+							<th>Player</th>
+							<th>YR</th>
+							<th>POS</th>
+							<th>HT</th>
+							<th>GP</th>
+							<th>FG%</th>
+							<th>3P%</th>
+							<th>FT%</th>
+							<th>Avg PTS</th>
+							<th>Avg REB</th>
+							<th>AST</th>
+							<th>STL</th>
+							<th>BLK</th>
+							<th>Fouls</th>
+						</tr>";
+					    // output data of each row
+					    while($row = mysqli_fetch_assoc($result)) {
+					        echo "<tr>";
+						    echo "<td>" . $row['jersey'] . "</td>";
+						    echo "<td>" . $row['player'] . "</td>";
+						    echo "<td>" . $row['yr'] . "</td>";
+						    echo "<td>" . $row['pos'] . "</td>";
+						    echo "<td>" . $row['ht'] . "</td>";
+						    echo "<td>" . $row['gp'] . "</td>";
+						    echo "<td>" . $row['fg%'] . "</td>";
+						    echo "<td>" . $row['3fg%'] . "</td>";
+						    echo "<td>" . $row['ft%'] . "</td>";
+						    echo "<td>" . $row['avgpts'] . "</td>";
+						    echo "<td>" . $row['avgreb'] . "</td>";
+						    echo "<td>" . $row['ast'] . "</td>";
+						    echo "<td>" . $row['stl'] . "</td>";
+						    echo "<td>" . $row['blk'] . "</td>";
+						    echo "<td>" . $row['fouls'] . "</td>";
+						    echo "</tr>";
+					    }
+						echo "</table>";
+					} else {
+					    echo "0 results";
+					}
+
+					mysqli_close($conn);
 				}
 
-				//make variable sql to hold the query
-				$sql = "SELECT jersey, player, yr, pos, ht, gp, `fg%`, `3fg%`, `ft%`, avgpts, avgreb, ast, stl, blk, fouls FROM `{$table}` ";
-				$result = mysqli_query($conn, $sql);//result of query
-
-				if (mysqli_num_rows($result) > 0) {
-					echo 
-					"<table>
-					<tr>
-						<th>Jersey</th>
-						<th>Player</th>
-						<th>YR</th>
-						<th>POS</th>
-						<th>HT</th>
-						<th>GP</th>
-						<th>FG%</th>
-						<th>3P%</th>
-						<th>FT%</th>
-						<th>Avg PTS</th>
-						<th>Avg REB</th>
-						<th>AST</th>
-						<th>STL</th>
-						<th>BLK</th>
-						<th>Fouls</th>
-					</tr>";
-				    // output data of each row
-				    while($row = mysqli_fetch_assoc($result)) {
-				        echo "<tr>";
-					    echo "<td>" . $row['jersey'] . "</td>";
-					    echo "<td>" . $row['player'] . "</td>";
-					    echo "<td>" . $row['yr'] . "</td>";
-					    echo "<td>" . $row['pos'] . "</td>";
-					    echo "<td>" . $row['ht'] . "</td>";
-					    echo "<td>" . $row['gp'] . "</td>";
-					    echo "<td>" . $row['fg%'] . "</td>";
-					    echo "<td>" . $row['3fg%'] . "</td>";
-					    echo "<td>" . $row['ft%'] . "</td>";
-					    echo "<td>" . $row['avgpts'] . "</td>";
-					    echo "<td>" . $row['avgreb'] . "</td>";
-					    echo "<td>" . $row['ast'] . "</td>";
-					    echo "<td>" . $row['stl'] . "</td>";
-					    echo "<td>" . $row['blk'] . "</td>";
-					    echo "<td>" . $row['fouls'] . "</td>";
-					    echo "</tr>";
-				    }
-					echo "</table>";
-				} else {
-				    echo "0 results";
-				}
-
-				mysqli_close($conn);
+				
 			?>
 
 		</div> 
